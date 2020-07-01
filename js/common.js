07c8f8909dbcb90f0ad331dd6c17cf64b739ff52
beautifuldecor <67161376+beautifuldecor@users.noreply.github.com>
2020-07-01 11:54:33 -0400

    Created API rows
    1. Taken all content and everyone pushed into array
    2. Split array for rows

--- js/common.js
+++ js/common.js
@@ -2,7 +2,8 @@ var doc = document;
 
 window.onload = function () {
     var loadMoreButton = doc.getElementsByClassName("more__more-btn")[0],
-        goodsContent   = doc.getElementsByClassName('goods__content')[0];
+        goodsContent   = doc.getElementsByClassName('goods__content')[0],
+        resultJSON = [];
 
     function loadMore() {
         goodsContent.style.height   = 'auto';
@@ -27,14 +28,36 @@ window.onload = function () {
         xhr.send();
     }
     getJSON('https://spreadsheets.google.com/feeds/cells/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/1/public/full?alt=json', function (err, data) {
-        console.log(err, data); 
-    })
+        paste(data.feed.entry, data.feed.entry.length);
+    }); 
+
+
+    function paste(_d, l) {
+        // Create array with all items
+        var itemsValues = [];
+
+        for (var i = 0; i <= l-1; i++) {
+            itemsValues.push(_d[i].gs$cell.inputValue);
+
+            if (i == l-1) {
+                createResult(itemsValues);
+            }
+        };
+
+        // Create arrays with rows
+        function createResult(list) {
+            var idx = 0;
+          
+            while (idx < list.length) {
+              if (idx % 8 === 0) resultJSON.push([])
+              resultJSON[resultJSON.length - 1].push(list[idx++])
+            }
+
+        };
+    };
+
+    console.log(resultJSON);
 }
 
 
 
-//https://spreadsheets.google.com/feeds/cells/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/1/public/full?alt=json
-// https://docs.google.com/spreadsheets/d/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/edit?alt=json
-// https://spreadsheets.google.com/fee ds/list/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/od6/public/values?alt=json
-// https://docs.google.com/spreadsheets/d/e/2PACX-1vTPxT-Isnte4poBEWx99a02hzBuEj4crGAxWeeAce4y63B5IWdgmqWckf2Fp2JlcbMknQgzslaNAk5s/pubhtml
-//// https://docs.google.com/spreadsheets/d/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/values?usp=sharing
