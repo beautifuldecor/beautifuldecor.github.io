7b08d57d424995b070daba8ac359c150e872c4fb
beautifuldecor <67161376+beautifuldecor@users.noreply.github.com>
2020-06-30 20:18:41 -0400

    Try connect to database, scroll. (Transform update)
    --- This is transform update for new laptop --- 
    Added few methods to connect to database, one work, but it get only cells (try fix it in new updates), confirmed scrolls to sections (try add new to next update).

--- js/common.js
+++ js/common.js
@@ -10,4 +10,31 @@ window.onload = function () {
     }
 
     loadMoreButton.addEventListener("click", loadMore);
+
+    // Request
+    let getJSON = function(url, callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            let status = xhr.status;
+            if (status == 200) {
+                callback(null, xhr.response)
+            } else {
+                callback(status, xhr.response)
+            }
+        };
+        xhr.send();
+    }
+    getJSON('https://spreadsheets.google.com/feeds/cells/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/1/public/full?alt=json', function (err, data) {
+        console.log(err, data); 
+    })
 }
+
+
+
+//https://spreadsheets.google.com/feeds/cells/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/1/public/full?alt=json
+// https://docs.google.com/spreadsheets/d/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/edit?alt=json
+// https://spreadsheets.google.com/fee ds/list/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/od6/public/values?alt=json
+// https://docs.google.com/spreadsheets/d/e/2PACX-1vTPxT-Isnte4poBEWx99a02hzBuEj4crGAxWeeAce4y63B5IWdgmqWckf2Fp2JlcbMknQgzslaNAk5s/pubhtml
+//// https://docs.google.com/spreadsheets/d/1JhYqoP99niOPAsUNpTLzUj0D9jx1Y3-Dfb_ps5Z3Cms/values?usp=sharing
