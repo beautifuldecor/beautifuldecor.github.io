9c5004e5bc5dc16bd83629e0455ed486d128a4cc
beautifuldecor <67161376+beautifuldecor@users.noreply.github.com>
2020-07-02 21:50:10 -0400

    Done work with all page
    Site correctly loads all API, if dev wants more speed tip: reload only some elements by using JS or HTML (closepost()). All page is adaptive, if dev wants more adaptive for post: adapt with libraries to Figma template.

--- js/common.js
+++ js/common.js
@@ -3,7 +3,8 @@ var doc = document;
 window.onload = function () {
     var goodsContent   = doc.getElementsByClassName('goods__content')[0],
         resultJSON = [],
-        contentItem = '';
+        contentItem = '',
+        foundedArray;
 
     // Request
     let getJSON = function(url, callback) {
@@ -43,43 +44,63 @@ window.onload = function () {
             var idx = 0;
           
             while (idx < list.length) {
-              if (idx % 8 === 0) resultJSON.push([])
+              if (idx % 9 === 0) resultJSON.push([])
               resultJSON[resultJSON.length - 1].push(list[idx++])
             }
 
             createItemsHTML(resultJSON);
-
         };
     };
 
     // Creating posts
     function createItemsHTML(resultJSON) {        
         for (var i = 1; i <= resultJSON.length-1; i++) {
-            contentItem = `<div class="content__item"><div class="item__photo"> <img src="${resultJSON[i]['3']}" alt="${resultJSON[i][1]}" class="photo__photo-item"></div><div class="item__info"><h1 class="item__title">${resultJSON[i][1]}</h1><p class="item__paragraph">${resultJSON[i][7]}</p> <a class="more-btn postMore">Show more</a></div></div>`
+            contentItem = `<div class="content__item"><div class="item__photo"> <img src="${resultJSON[i]['3']}" alt="${resultJSON[i][1]}" class="photo__photo-item"></div><div class="item__info"><h1 class="item__title">${resultJSON[i][1]}</h1><p class="item__paragraph">${resultJSON[i][7]}</p> <a class="more-btn postMore" id="${resultJSON[i][8]}">Show more</a></div></div>`
             goodsContent.innerHTML += contentItem;
         };
 
-        function seeMore() {
-            contentPost = `<div class="detailForPost"><div class="detailForPost__photo"><div> <img src="img/post1.jpg" alt=""></div></div><div class="detailForPost__info"><div class="container"><div class="info__centering"><div class="infobox__title"><h4 class="title__small">Tne new way in</h4><h1 class="title__big green">Spring decor</h1></div><p class="post-infobox post-infobox-p"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum ligula erat, nec venenatis neque sodales eu. Nunc imperdiet dignissim ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum ligula erat, nec venenatis neque sodales eu. Nunc imperdiet dignissim ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum ligula erat, nec venenatis neque sodales eu. Nunc imperdiet dignissim ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum ligula erat, nec venenatis neque sodales eu. Nunc imperdiet dignissim ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum ligula erat, nec venenatis neque sodales eu. Nunc imperdiet dignissim ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum ligula erat, nec venenatis neque sodales eu. Nunc imperdiet dignissim ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum ligula erat, nec venenatis neque sodales eu. Nunc imperdiet dignissim ante.</p> <a>See more imagies</a> <br> <br> <a class="closePostBTN">Close me</a></div></div></div></div>`
+
+        var elements = document.getElementsByClassName("postMore");
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            elements[i].onclick = function(element){
+                seeMore(element);
+            };
+        };
+
+        function seeMore(element) {
+            var elementID = element.path[0].id;
+            console.log(resultJSON, elementID)
+
+
+            for(var i = 0; i <= elements.length; i++) {
+                const found = resultJSON[i].find(element => element === elementID);
+                if(found != undefined) {
+                    foundedArray = resultJSON[i];
+                    console.log(resultJSON[i]);
+                }
+            }
+
+            
+
+            contentPost = `<div class="detailForPost"><div class="detailForPost__photo"><div> <img src="${foundedArray[3]}" alt=""></div></div><div class="detailForPost__info"><div class="container"><div class="info__centering"><div class="infobox__title"><h4 class="title__small">Tne new way in</h4><h1 class="title__big green">${foundedArray[1]}</h1></div><p class="post-infobox post-infobox-p">${foundedArray[6]}</p> <a href="">See more imagies</a> <br> <br> <a class="closePostBTN">Close me</a></div></div></div></div>`
             doc.body.innerHTML += contentPost;
+
+
+
             doc.getElementsByTagName("HEADER")[0].style.display = 'none';
             doc.getElementsByTagName("FOOTER")[0].style.display = 'none';
             doc.getElementsByTagName("SECTION")[0].style.display = 'none';
             doc.getElementsByTagName("SECTION")[1].style.display = 'none';
 
-
             function closePost() {
-                doc.getElementsByTagName("HEADER")[0].style.display = 'block';
-                doc.getElementsByTagName("FOOTER")[0].style.display = 'block';
-                doc.getElementsByTagName("SECTION")[0].style.display = 'block';
-                doc.getElementsByTagName("SECTION")[1].style.display = 'block';
-                doc.getElementsByClassName("detailForPost")[0].style.display = 'none';
-                doc.getElementsByClassName("more__more-btn")[0].addEventListener("click", loadMore);
+                location.reload();
+                // If you want update it: reload only some elements by using JS or HTML
             };
             doc.getElementsByClassName('closePostBTN')[0].addEventListener("click", closePost);
         };
-
-        doc.getElementsByClassName('postMore')[0].addEventListener("click", seeMore);
+        
+        
     };
     
     function loadMore() {
